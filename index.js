f1f4266887c6db8c6fd25d9e306a694f759e1313
Sean Aird <seanraird@yahoo.com>
2021-06-22 11:21:48 -0400

    commit tree d5ffce1c525ac02ba12bdb676dccfec5a762a134

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const divide = (num1, num2) => num1 / num2; 
+
+divide(2000,100);
+
+const square = (parameter) => parameter ** 2;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
